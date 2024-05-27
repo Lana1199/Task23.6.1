92db5f6ac5c0003e6cd9e5d27f496440287c3407
Lana1199 <161425027+Lana1199@users.noreply.github.com>
2024-05-27 23:08:36 +0300

    Update extensions.py

--- extensions.py
+++ extensions.py
@@ -28,5 +28,5 @@ class CriptoConverter:
 
         r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
         total_base = json.loads(r.content)[keys[base]]
-
-        return total_base
+       
+        return total_base * amount
