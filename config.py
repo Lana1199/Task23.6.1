381387ef412aae8f0efa31c3016d3ba81c1d6d7f
Lana1199 <161425027+Lana1199@users.noreply.github.com>
2024-05-27 15:07:26 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+
+TOKEN = '7148003001:AAGdcjZF4qbfMPJDZDwpZAVwCbjSX0aS4gg'
+
+keys = {
+    'рубль': 'RUB',
+    'доллар': 'USD',
+    'евро': 'EUR',
+}
---
+++ extensions.py
@@ -0,0 +1,32 @@
+import requests
+import json
+from config import keys
+
+
+class APIExcetion(Exception):
+    pass
+
+
+class CriptoConverter:
+    @staticmethod
+    def get_price(quote: str, base: str, amount: str):
+
+        if quote == base:
+            raise APIExcetion(f'Невозможно перевести одинаковые валюты{base}.')
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise APIExcetion(f'Не удалось обработать валюту {quote}')
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise APIExcetion(f'Не удалось обработать валюту {base}')
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise APIExcetion(f'Не удалось обработать количество валюты {amount}')
+
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
+        total_base = json.loads(r.content)[keys[base]]
+
+        return total_base
